ccd6b0468eacea9d86408169dfd72c2ce01dde7d
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-08-11 14:59:53 -0300

    chore: deleta arquivos criados automaticamente (#39)

---
+++ Presentation/PrintsControl.WebApi/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+using MediatR;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using PrintsControl.Application.UseCases.CreateUser;
+
+namespace PrintsControl.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[EnableCors("AllowAll")]
+// [Authorize]
+public class UserController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public UserController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<CreateUserResponse>> Create(CreateUserCommand request,CancellationToken cancellationToken)
+    {
+        var validator = new CreateUserValidator();
+        var validationResult = await validator.ValidateAsync(request);
+
+        if (!validationResult.IsValid)
+        {
+            return BadRequest(validationResult.Errors);
+        }
+        
+        var response = await _mediator.Send(request, cancellationToken);
+        return Ok(response);
+    }
+}
