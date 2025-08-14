67f885262d79e2bb7f5b8b928ae72d40312a292d
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-08-14 10:55:32 -0300

    feat: Mapper busca por usuario por id (#66)

---
+++ Core/PrintsControl.Application/Shared/Mappings/Users/GetByIdUserMapper.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using PrintsControl.Application.Dtos.Users;
+using PrintsControl.Domain.Entities;
+
+namespace PrintsControl.Application.Shared.Mappings.Users;
+
+public class GetByIdUserMapper : Profile
+{
+    public GetByIdUserMapper()
+    {
+        CreateMap<User, GetByIdUserResponse>();
+    }
+}
