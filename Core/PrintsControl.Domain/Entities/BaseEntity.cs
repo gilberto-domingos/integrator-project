183471c2997cd011d3573b95ede043002fe42d33
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-08-15 14:00:50 -0300

    refactor: entidade base mais complementada (#68)

--- Core/PrintsControl.Domain/Entities/BaseEntity.cs
+++ Core/PrintsControl.Domain/Entities/BaseEntity.cs
@@ -1,10 +1,29 @@
-namespace PrintsControl.Domain.Entities;
+using System;
 
-public abstract class BaseEntity
+namespace PrintsControl.Domain.Entities
 {
-    public Guid Id { get; set; }
-    public DateTimeOffset CreatedAt { get; set; }
-    public DateTimeOffset UpdatedAt { get; set; } 
-    public DateTimeOffset? DeletedAt { get; set; }
-    
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; protected set; }
+        public DateTimeOffset CreatedAt { get; protected set; }
+        public DateTimeOffset UpdatedAt { get; protected set; }
+        public DateTimeOffset? DeletedAt { get; protected set; }
+
+        protected BaseEntity()
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = DateTimeOffset.UtcNow;
+            UpdatedAt = DateTimeOffset.UtcNow;
+        }
+
+        public void MarkAsUpdated()
+        {
+            UpdatedAt = DateTimeOffset.UtcNow;
+        }
+
+        public void MarkAsDeleted()
+        {
+            DeletedAt = DateTimeOffset.UtcNow;
+        }
+    }
 }
