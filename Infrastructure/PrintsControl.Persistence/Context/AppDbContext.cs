b86c1f3cb5fa8e057492c6808c9f94b28e82d0d1
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-08-15 15:39:01 -0300

    chore: registra classe student no contexto (#72)

--- Infrastructure/PrintsControl.Persistence/Context/AppDbContext.cs
+++ Infrastructure/PrintsControl.Persistence/Context/AppDbContext.cs
@@ -13,6 +13,8 @@ public class AppDbContext : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<Transaction> Transactions { get; set; }
     
+    public DbSet<Student>Students { get; set; }
+    
    protected override void OnModelCreating(ModelBuilder modelBuilder)
 {
     base.OnModelCreating(modelBuilder);
