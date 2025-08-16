9d3ea0676de76b5f0d3a449e205a66502a9f1e46
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-08-16 02:09:17 -0300

    chore: registra as fluent api classes no AppDbContext (#78)

--- Infrastructure/PrintsControl.Persistence/Context/AppDbContext.cs
+++ Infrastructure/PrintsControl.Persistence/Context/AppDbContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using PrintsControl.Domain.Entities;
+using PrintsControl.Persistence.Configurations.cs;
 
 namespace PrintsControl.Persistence.Context;
 
@@ -15,65 +16,21 @@ public class AppDbContext : DbContext
     
     public DbSet<Student>Students { get; set; }
     
-   protected override void OnModelCreating(ModelBuilder modelBuilder)
-{
-    base.OnModelCreating(modelBuilder);
-
-    foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        if (typeof(BaseEntity).IsAssignableFrom(entityType.ClrType))
-        {
-            modelBuilder.Entity(entityType.ClrType)
-                .Property<DateTimeOffset>("CreatedAt")
-                .IsRequired();
+        base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity(entityType.ClrType)
-                .Property<DateTimeOffset>("UpdatedAt")
-                .IsRequired();
-
-            modelBuilder.Entity(entityType.ClrType)
-                .Property<DateTimeOffset?>("DeletedAt")
-                .IsRequired(false);
-
-            var method = typeof(AppDbContext)
-                .GetMethod(nameof(SetSoftDeleteFilter), System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static)!
-                .MakeGenericMethod(entityType.ClrType);
-
-            method.Invoke(null, new object[] { modelBuilder });
-        }
+        modelBuilder.ApplyConfiguration(new FluentStudentConfiguration());
+        modelBuilder.ApplyConfiguration(new FluentPurchaseConfiguration());
+        modelBuilder.ApplyConfiguration(new FluentPrintingConfiguration());
+        modelBuilder.ApplyConfiguration(new FluentTransactionHistoryConfiguration());
+        modelBuilder.ApplyConfiguration(new FluentUserConfiguration());
+        modelBuilder.ApplyConfiguration(new FluentTransactionConfiguration());
     }
-
-    modelBuilder.Entity<Transaction>()
-        .HasOne(t => t.User)
-        .WithMany(u => u.Transactions)
-        .HasForeignKey(t => t.UserId)
-        .IsRequired(false);
-
-    modelBuilder.Entity<User>()
-        .Property(u => u.Email)
-        .HasMaxLength(150)
-        .IsRequired();
-
-    modelBuilder.Entity<User>()
-        .Property(u => u.Password)
-        .HasMaxLength(100)
-        .IsRequired();
-
-    modelBuilder.Entity<Transaction>()
-        .Property(t => t.Type)
-        .HasMaxLength(50)
-        .IsRequired();
-
-    modelBuilder.Entity<Transaction>()
-        .Property(t => t.Category)
-        .HasMaxLength(100)
-        .IsRequired();
-}
-
-private static void SetSoftDeleteFilter<TEntity>(ModelBuilder builder) where TEntity : BaseEntity
-{
-    builder.Entity<TEntity>().HasQueryFilter(e => e.DeletedAt == null);
-}
+    private static void SetSoftDeleteFilter<TEntity>(ModelBuilder builder) where TEntity : BaseEntity
+    {
+        builder.Entity<TEntity>().HasQueryFilter(e => e.DeletedAt == null);
+    }
 
     
 }
