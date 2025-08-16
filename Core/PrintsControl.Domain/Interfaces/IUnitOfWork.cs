aa0724a6f8ec8028710fddfb3e6222c30576464b
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-08-16 18:00:54 -0300

    refactor: Injeta os repositórios no unitOfWork (#94)

--- Core/PrintsControl.Domain/Interfaces/IUnitOfWork.cs
+++ Core/PrintsControl.Domain/Interfaces/IUnitOfWork.cs
@@ -2,5 +2,8 @@ namespace PrintsControl.Domain.Interfaces;
 
 public interface IUnitOfWork
 {
+    IStudentRepository Students { get; }
+    IUserRepository Users { get; }
+    ITransactionRepository Transactions { get; }
     Task CommitAsync(CancellationToken cancellationToken);
 }
--- Infrastructure/PrintsControl.Persistence/Repositories/UnitOfWork.cs
+++ Infrastructure/PrintsControl.Persistence/Repositories/UnitOfWork.cs
@@ -3,13 +3,24 @@ using PrintsControl.Persistence.Context;
 
 namespace PrintsControl.Persistence.Repositories;
 
-public class UnitOfWork : IUnitOfWork
+public sealed class UnitOfWork : IUnitOfWork,  IDisposable
 {
     private readonly AppDbContext _context;
+    
+    public IStudentRepository Students { get; }
+    public IUserRepository Users { get; }
+    public ITransactionRepository Transactions { get; }
 
-    public UnitOfWork(AppDbContext context)
+    public UnitOfWork(
+        AppDbContext context,
+        IStudentRepository studentRepository,
+        IUserRepository userRepository,
+        ITransactionRepository transactionRepository)
     {
         _context = context;
+        Students = studentRepository;
+        Users = userRepository;
+        Transactions = transactionRepository;
     }
 
     public async Task CommitAsync(CancellationToken cancellationToken)
@@ -17,6 +28,9 @@ public class UnitOfWork : IUnitOfWork
         await _context.SaveChangesAsync(cancellationToken);
     }
     
-    
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
 
 }
